f2a12f9d1fd49e79f4f466cf8a00f63648ed7ca2
pective <139483386+pective@users.noreply.github.com>
2025-02-17 19:36:17 +0100

    fixed

--- learncppCourse/Chapter4/unsignedInt/uAndS/main.cpp
+++ learncppCourse/Chapter4/unsignedInt/uAndS/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     signed int s { -1 };
-    unsigned int u { 1 };
+    signed int u { 1 };
 
     if (s < u) // -1 is implicitly converted to 4294967295, and 4294967295 < 1 is false
         std::cout << "-1 is less than 1\n";
